c49b1f916430caa856bd2b752836971d9d785c03
Saroj Patro <sarojkumar.patro@siemens.com>
2023-12-16 04:24:48 +0000

    Code challenge submission for question: 242 Valid Anagram

---
+++ 242 Valid Anagram/242valid-anagram.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size())
+        {
+            return false;
+        }
+
+        int arr[26] = {0};
+
+        for(int i=0; i< s.size();i++)
+        {
+            arr[s[i] - 'a'] += 1;
+            arr[t[i] - 'a'] -= 1;
+        }
+        
+        for(int i=0; i < 26; i++)
+        {
+            if(arr[i] != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
