735d1fbfdbacbaf089e5b9fb0da25a1935f59cdb
Saroj Patro <sarojkumar.patro@siemens.com>
2023-12-21 13:38:52 +0000

    Code challenge submission for question: 1637 Widest Vertical Area Between Two Points Containing No Points

---
+++ 1637 Widest Vertical Area Between Two Points Containing No Points/1637widest-vertical-area-between-two-points-containing-no-points.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxWidthOfVerticalArea(vector<vector<int>>& points) {
+
+        sort(points.begin(), points.end(), [] (const auto& a, const auto& b){
+            return a[0] < b[0];
+        });
+
+        int res = 0;
+        for (int i=1; i<points.size(); i++)
+        {
+            res = max(res, points[i][0]-points[i-1][0]);
+        }
+
+        return res;
+    }
+};
