7f7db3fa94e451950e0127858ea976675bd12f12
Saroj Patro <sarojkumar.patro@siemens.com>
2024-03-19 18:20:42 +0000

    Code challenge submission for question: 621 Task Scheduler

---
+++ 621 Task Scheduler/621task-scheduler.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        // Create a frequency array to keep track of the count of each task
+        vector<int> freq(26);
+        for (char task : tasks) {
+            freq[task - 'A']++;
+        }
+        // Sort the frequency array in non-decreasing order
+        sort(freq.begin(), freq.end());
+        // Calculate the maximum frequency of any task
+        int maxFreq = freq[25] - 1;
+        // Calculate the number of idle slots that will be required
+        int idleSlots = maxFreq * n;
+        cout << idleSlots << endl;
+        // Iterate over the frequency array from the second highest frequency to the lowest frequency
+        for (int i = 24; i >= 0 && freq[i] > 0; i--) {
+            
+            // Subtract the minimum of the maximum frequency and the current frequency from the idle slots
+            idleSlots -= min(maxFreq, freq[i]);
+            
+        }
+        // If there are any idle slots left, add them to the total number of tasks
+        return idleSlots > 0 ? idleSlots + tasks.size() : tasks.size();
+    }
+};
